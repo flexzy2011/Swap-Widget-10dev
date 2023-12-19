fc23916bfd0b62b75a3176cad52452878241cda8
felix akinloye <felixakinloye247@gmail.com>
2023-12-19 15:27:55 +0100

    Swap Widget 10dev

---
+++ swap-widget-10dev/src/components/FirstFrame.js
@@ -0,0 +1,162 @@
+import "./FirstFrame.css";
+
+const FirstFrame = () => {
+  return (
+    <div className="div">
+      <div className="inner">
+        <div className="frame-parent">
+          <div className="frame-group">
+            <div className="frame-container">
+              <div className="mayan-swap-parent">
+                <div className="mayan-swap">Swap Widget 10dev</div>
+                <div className="powered-by-serum">(Powered by Serum)</div>
+              </div>
+              <div className="instance-parent">
+                <div className="eth-parent">
+                  <div className="eth">0.43 ETH</div>
+                  <div className="div1">|</div>
+                  <div className="x5f3beb4d-parent">
+                    <div className="x5f3beb4d">0x5F...EB4D</div>
+                    <img className="avatar-icon" alt="" src="/image/Avatar.png" />
+                  </div>
+                </div>
+                <div className="settings-wrapper">
+                  <img
+                    className="settings-icon"
+                    alt=""
+                    src="/image/Settings.png"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="frame-div">
+              <div className="instance-group">
+                <div className="frame-parent1">
+                  <div className="frame-parent2">
+                    <div className="instance-wrapper">
+                      <div className="frame-parent3">
+                        <div className="crypto-parent">
+                          <img
+                            className="settings-icon"
+                            alt=""
+                            src="/image/Vector.png"
+                          />
+                          <div className="eth1">ETH</div>
+                        </div>
+                        <img
+                          className="settings-icon"
+                          alt=""
+                          src="/image/Chevron.png"
+                        />
+                      </div>
+                    </div>
+                    <div className="instance-wrapper">
+                      <div className="frame-parent3">
+                        <div className="crypto-parent">
+                          <img
+                            className="settings-icon"
+                            alt=""
+                            src="/image/solana-sol-seeklogo.com 1.png"
+                          />
+                          <div className="eth1">Solana</div>
+                        </div>
+                        <img
+                          className="settings-icon"
+                          alt=""
+                          src="/image/Chevron.png"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="frame-parent5">
+                    <div className="parent">
+                      <div className="div2">2.49</div>
+                      <div className="eth1">≈$6341.4</div>
+                      <div className="div4">≈0.00</div>
+                    </div>
+                    <div className="frame-parent6">
+                      <div className="max-wrapper">
+                        <div className="max">MAX</div>
+                      </div>
+                      <div className="frame-child" />
+                      <div className="balance-0">Balance: 0</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="frame-parent1">
+                  <div className="frame-parent2">
+                    <div className="instance-wrapper">
+                      <div className="frame-parent3">
+                        <div className="crypto-parent">
+                          <img
+                            className="settings-icon"
+                            alt=""
+                            src="/image/Crypto.png"
+                          />
+                          <div className="eth1">BTC</div>
+                        </div>
+                        <img
+                          className="settings-icon"
+                          alt=""
+                          src="/image/Chevron.png"
+                        />
+                      </div>
+                    </div>
+                    <div className="instance-wrapper">
+                      <div className="frame-parent3">
+                        <div className="crypto-parent">
+                          <img
+                            className="settings-icon"
+                            alt=""
+                            src="/image/polygon-matic-logo 1.png"
+                          />
+                          <div className="eth1">Polygan</div>
+                        </div>
+                        <img
+                          className="settings-icon"
+                          alt=""
+                          src="/image/Chevron.png"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="frame-parent5">
+                    <div className="parent">
+                      <div className="div2">0.353</div>
+                      <div className="eth1">≈$6339.2</div>
+                      <div className="eth1">(-0.5%)</div>
+                    </div>
+                    <div className="frame-parent6">
+                      <div className="max-container">
+                        <div className="max">Balance: 0.04</div>
+                      </div>
+                      <div className="frame-item" />
+                      <div className="balance-0">Balance: 0</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="arrow-wrapper">
+                <img className="arrow-icon" alt="" src="/image/Arrow.png" />
+              </div>
+            </div>
+          </div>
+          <div className="swap-info-touchpoint">
+            <div className="eth-00984">1 ETH = 0.0984 BTC</div>
+            <div className="fee-2252-parent">
+              <div className="eth-00984">Fee : 22.52$</div>
+              <img className="arrow-icon" alt="" src="/image/arrow-right.png" />
+            </div>
+          </div>
+          <div className="button">
+            <img className="blank-icon" alt="" src="/blank@2x.png" />
+            <b className="text">Swap</b>
+            <img className="blank-icon" alt="" src="/blank@2x.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FirstFrame;
